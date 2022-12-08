6e7d67fb5c04f145ca377d14630a8ac00726fcfb
Krzysztof Kamycki <krzysiekk09@o2.pl>
2022-12-08 10:15:33 +0100

    8 - refactor

--- 8/8b.py
+++ 8/8b.py
@@ -11,56 +11,48 @@ forest = []
 for line in data:
     forest.append([int(x) for x in line])
 
-max_scenic = 1
-for x in range(0, len(forest)):
-    for y in range(0, len(forest[x])):
+max_scenic = 0
+for x in range(1, len(forest) - 1):
+    for y in range(1, len(forest[x]) - 1):
         height = forest[x][y]
         current_scenic = 1
         # check left
         direction_score = 0
-        if y - 1 < 0:
-            continue
-        else:
-            for i in range(y - 1, -1, -1):
-                direction_score += 1
-                if forest[x][i] < height:
-                    continue
-                break
+        for i in range(y - 1, -1, -1):
+            direction_score += 1
+            if forest[x][i] < height:
+                continue
+            break
+
         current_scenic *= direction_score
         # check right
         direction_score = 0
-        if y + 1 > len(forest[x]):
-            continue
-        else:
-            for i in range(y + 1, len(forest[x])):
-                direction_score += 1
-                if forest[x][i] < height:
-                    continue
-                break
+        for i in range(y + 1, len(forest[x])):
+            direction_score += 1
+            if forest[x][i] < height:
+                continue
+            break
+
         current_scenic *= direction_score
 
         column = get_column(forest, y)
         # check up
         direction_score = 0
-        if x - 1 < 0:
-            continue
-        else:
-            for i in range(x - 1, -1, -1):
-                direction_score += 1
-                if column[i] < height:
-                    continue
-                break
+        for i in range(x - 1, -1, -1):
+            direction_score += 1
+            if column[i] < height:
+                continue
+            break
+
         current_scenic *= direction_score
         # check down
         direction_score = 0
-        if x + 1 > len(column):
-            continue
-        else:
-            for i in range(x + 1, len(column)):
-                direction_score += 1
-                if column[i] < height:
-                    continue
-                break
+        for i in range(x + 1, len(column)):
+            direction_score += 1
+            if column[i] < height:
+                continue
+            break
+
         current_scenic *= direction_score
 
         if max_scenic < current_scenic:
